1f487b41db1deed4bc540f38d6697b8feed395c2
HeideQueen <lucyfabz@gmail.com>
2019-08-31 02:39:17 -0500

    finish game

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
 // Variables
 
+var easy = false;
 var colors = paletteGen(6);
 var goalColor = goalGen();
-var easy = false;
 var goalDisplay = document.querySelector("#goalColorDisplay");
 var statusDisplay = document.querySelector("#status");
 var h1Display = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var resetButton = document.querySelector("#reset");
-var easyButton = document.querySelector("#easy");
-var hardButton = document.querySelector("#hard");
 var easyMode = document.getElementsByClassName("hardMode");
+var modeBtn = document.querySelectorAll(".mode")
+
+// Initialize
+
+function gameStart() {
+    mainLoop();
+    btnControl();
+    goalDisplay.textContent = goalColor;
+    resetButton.addEventListener("click", function () {
+        resetGame(6);
+    });
+};
+
+gameStart();
 
 // Functions
+
+function mainLoop() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", function () {
+            var pickedColor = this.style.backgroundColor;
+            if (pickedColor === goalColor) {
+                statusDisplay.textContent = "Correct!";
+                changeColor(goalColor);
+                resetButton.textContent = "Play Again?";
+            } else {
+                this.style.backgroundColor = "#232323";
+                statusDisplay.textContent = "Try Again";
+            }
+        });
+    };
+}
+
+function btnControl() {
+    for (var i = 0; i < modeBtn.length; i++) {
+        modeBtn[i].addEventListener("click", function () {
+            modeBtn[0].classList.remove("selected");
+            modeBtn[1].classList.remove("selected");
+            this.classList.add("selected");
+            if (this.textContent === "Easy") {
+                resetGame(3);
+                for (var i = 0; i < easyMode.length; i++) {
+                    easyMode[i].style.display = "none";
+                }
+                easy = true;
+            } else {
+                resetGame(6);
+                easy = false;
+                for (var i = 0; i < easyMode.length; i++) {
+                    easyMode[i].style.display = "block";
+                }
+            }
+        })
+    };
+}
 
 function changeColor(color) {
     for (var i = 0; i < squares.length; i++) {
@@ -55,57 +107,3 @@ function resetGame(n) {
     statusDisplay.textContent = "";
     gameStart();
 }
-
-// Display & Status
-
-goalDisplay.textContent = goalColor;
-
-// Buttons
-
-resetButton.addEventListener("click", function () {
-    resetGame(6);
-    easy = false;
-    easyButton.classList.remove("selected");
-    hardButton.classList.add("selected");
-})
-
-easyButton.addEventListener("click", function () {
-    resetGame(3);
-    for (var i = 0; i < easyMode.length; i++) {
-        easyMode[i].style.display = "none";
-    }
-    easyButton.classList.add("selected");
-    hardButton.classList.remove("selected");
-    easy = true;
-})
-
-hardButton.addEventListener("click", function () {
-    resetGame(6);
-    easy = false;
-    easyButton.classList.remove("selected");
-    hardButton.classList.add("selected");
-})
-
-// Main Loop
-
-function gameStart() {
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].addEventListener("click", function () {
-            var pickedColor = this.style.backgroundColor;
-            if (pickedColor === goalColor) {
-                statusDisplay.textContent = "Correct!";
-                changeColor(goalColor);
-                resetButton.textContent = "Play Again?";
-            } else {
-                this.style.backgroundColor = "#232323";
-                statusDisplay.textContent = "Try Again";
-            }
-        });
-    };
-    for (var i = 0; i < easyMode.length; i++) {
-        easyMode[i].style.display = "block";
-    }
-};
-
-gameStart();
